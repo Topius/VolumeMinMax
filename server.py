d000e8341fefec2d189cb862acef91d87334c019
Topius <34198115+Topius@users.noreply.github.com>
2024-04-03 03:09:25 +0400

    Update server.py

--- server.py
+++ server.py
@@ -242,5 +242,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create the tables if they don't exist
     threading.Thread(target=schedule_update).start()
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
